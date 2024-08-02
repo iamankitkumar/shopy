fe61d868824b61a364312f3c7da6f5119cccbecf
iamankitkumar <yadavankitkumar6600@gmail.com>
2024-08-02 21:02:07 +0530

    uu

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+       <li>Home</li>
+       <li>About</li>
+       <li>Content</li>
+       <li>services</li>
+        </ul>
+    </nav>
+    </div>
+  )
+}
+
+export default Navbar
